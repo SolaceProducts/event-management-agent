cff3d437758afdf7f1acaa4f3ce0a29d5d531b87
Habes <abdullah.habes@solace.com>
2022-09-11 13:43:54 -0400

    add Swagger unit test

---
+++ service/application/src/test/java/com/solace/maas/ep/runtime/agent/scanManager/swagger/SwaggerConfigTest.java
@@ -0,0 +1,32 @@
+package com.solace.maas.ep.runtime.agent.scanManager.swagger;
+
+import com.solace.maas.ep.runtime.agent.TestConfig;
+import com.solace.maas.ep.runtime.agent.config.SwaggerConfig;
+import lombok.SneakyThrows;
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.core.io.ResourceLoader;
+import org.springframework.test.context.ActiveProfiles;
+
+import static org.assertj.core.api.Assertions.assertThatNoException;
+
+@ActiveProfiles("TEST")
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT, classes = TestConfig.class)
+public class SwaggerConfigTest {
+
+    @InjectMocks
+    SwaggerConfig swaggerConfig;
+
+    @Autowired
+    ResourceLoader resourceLoader;
+
+    @Test
+    @SneakyThrows
+    public void SwaggerConfigTest() {
+        swaggerConfig.swagger(resourceLoader);
+
+        assertThatNoException();
+    }
+}
