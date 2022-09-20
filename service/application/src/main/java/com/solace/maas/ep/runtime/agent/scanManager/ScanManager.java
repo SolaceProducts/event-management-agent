13d24e6df8cdc64f501e328674ba74e18b668ecd
Omar Mostafa <Omostafa@omarmostafa.local>
2022-09-20 19:12:21 -0400

    Removed un used import

--- service/application/src/main/java/com/solace/maas/ep/runtime/agent/scanManager/ScanManager.java
+++ service/application/src/main/java/com/solace/maas/ep/runtime/agent/scanManager/ScanManager.java
@@ -14,7 +14,6 @@ import org.springframework.stereotype.Service;
 import java.util.List;
 import java.util.Objects;
 import java.util.stream.Collectors;
-import java.util.stream.Stream;
 
 @Slf4j
 @Service
