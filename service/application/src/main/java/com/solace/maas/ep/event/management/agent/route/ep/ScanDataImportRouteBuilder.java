fc81f92bc7b4ba129c61b133cc53960b1f7f6016
Habes <abdullah.habes@solace.com>
2022-10-27 12:27:40 -0400

    update ScanDataImportRouteBuilder to aggregate the files list

--- service/application/src/main/java/com/solace/maas/ep/event/management/agent/route/ep/ScanDataImportRouteBuilder.java
+++ service/application/src/main/java/com/solace/maas/ep/event/management/agent/route/ep/ScanDataImportRouteBuilder.java
@@ -1,16 +1,23 @@
 package com.solace.maas.ep.event.management.agent.route.ep;
 
 import com.solace.maas.ep.event.management.agent.plugin.constants.RouteConstants;
+import com.solace.maas.ep.event.management.agent.plugin.constants.ScanStatus;
+import com.solace.maas.ep.event.management.agent.plugin.constants.ScanStatusType;
 import com.solace.maas.ep.event.management.agent.processor.ScanDataImportAllFilesProcessor;
 import com.solace.maas.ep.event.management.agent.processor.ScanDataImportGroupFilesProcessor;
 import com.solace.maas.ep.event.management.agent.processor.ScanDataImportProcessor;
 import com.solace.maas.ep.event.management.agent.processor.ScanDataImportScanFilesProcessor;
 import com.solace.maas.ep.event.management.agent.route.ep.exceptionHandlers.ScanDataImportExceptionHandler;
+import org.apache.camel.AggregationStrategy;
+import org.apache.camel.Exchange;
 import org.apache.camel.builder.RouteBuilder;
 import org.apache.camel.dataformat.zipfile.ZipSplitter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnExpression;
 import org.springframework.stereotype.Component;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import static org.apache.camel.support.builder.PredicateBuilder.and;
 
@@ -41,25 +48,45 @@ public class ScanDataImportRouteBuilder extends RouteBuilder {
     public void configure() {
         from("seda:manualImport?blockWhenFull=true&size=100")
                 .routeId("manualImport")
+                .setHeader(RouteConstants.AGGREGATION_ID, constant("id"))
                 .onException(Exception.class)
                 .process(new ScanDataImportExceptionHandler())
                 .continued(true)
                 .end()
                 .split(new ZipSplitter())
                 .streaming()
+                .convertBodyTo(String.class)
+
                 .choice()
+                // Covers the scenario where schedule id scan id and scan are specified.
                 .when(and(header("CamelFileName").contains(header(RouteConstants.SCHEDULE_ID)),
                         header("CamelFileName").contains(header(RouteConstants.SCAN_ID))))
-                .convertBodyTo(String.class)
+                .aggregate(header("CamelFileName"), new ArrayListAggregationStrategy())
+                .completionTimeout(500)
+                .setHeader(RouteConstants.SCAN_STATUS, constant(ScanStatus.IN_PROGRESS))
+                .setHeader(RouteConstants.SCAN_STATUS_TYPE, constant(ScanStatusType.PER_ROUTE))
                 .process(scanDataImportScanFilesProcessor)
+                .endChoice()
+
+                // Covers the scenario where only schedule is specified. All scanId folders in the schedule directory will be processed.
                 .when(and(header("CamelFileName").contains(header(RouteConstants.SCHEDULE_ID)),
                         header(RouteConstants.SCAN_ID).isNull()))
-                .convertBodyTo(String.class)
+                .aggregate(header("CamelFileName"), new ArrayListAggregationStrategy())
+                .completionTimeout(500)
+                .setHeader(RouteConstants.SCAN_STATUS, constant(ScanStatus.IN_PROGRESS))
+                .setHeader(RouteConstants.SCAN_STATUS_TYPE, constant(ScanStatusType.PER_ROUTE))
                 .process(scanDataImportGroupFilesProcessor)
+                .endChoice()
+
+                // Nothing is specified. Imports all scan data and log data
                 .when(and(header(RouteConstants.SCHEDULE_ID).isNull(), header(RouteConstants.SCAN_ID).isNull()))
-                .convertBodyTo(String.class)
+                .aggregate(header("CamelFileName"), new ArrayListAggregationStrategy())
+                .completionTimeout(500)
+                .setHeader(RouteConstants.SCAN_STATUS, constant(ScanStatus.IN_PROGRESS))
+                .setHeader(RouteConstants.SCAN_STATUS_TYPE, constant(ScanStatusType.PER_ROUTE))
                 .process(scanDataImportAllFilesProcessor)
                 .endChoice()
+
                 .end();
 
         from("seda:importDataPublisher?blockWhenFull=true&size=100")
@@ -69,5 +96,17 @@ public class ScanDataImportRouteBuilder extends RouteBuilder {
                 .process(new ScanDataImportExceptionHandler())
                 .continued(true)
                 .end();
+    }
+
+    public static class ArrayListAggregationStrategy implements AggregationStrategy {
+        List<String> fileList = new ArrayList<>();
+
+        @Override
+        public Exchange aggregate(Exchange oldExchange, Exchange newExchange) {
+            String filename = (String) newExchange.getIn().getHeader("CamelFileName");
+            fileList.add(filename);
+            newExchange.getIn().setHeader("FILE_NAMES", fileList);
+            return newExchange;
+        }
     }
 }
