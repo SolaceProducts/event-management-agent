001fc0e90b951e978a684cce2d38b87d7706c83e
shooshmand-sol <salman.hooshmand@solace.com>
2023-05-15 11:39:16 -0400

    wip

--- service/application/src/main/java/com/solace/maas/ep/event/management/agent/EMAApplication.java
+++ service/application/src/main/java/com/solace/maas/ep/event/management/agent/EMAApplication.java
@@ -21,8 +21,8 @@ import java.util.Properties;
 @ComponentScan({"com.solace.maas"})
 @EnableScheduling
 @EnableJpaAuditing
-public class EMAApplication {                                                                                                                                                                                
-
+public class EMAApplication {
+  
     protected static final Map<String, Object> startupStartedMarker = new HashMap<>();
     protected static final Map<String, Object> startupCompletedMarker = new HashMap<>();
     private static final Logger log = LoggerFactory.getLogger(EMAApplication.class);
