68a337e96d7d39b82d279a468a354bb0a4ebc411
Puck Wang <puck.wang@solace.com>
2023-05-30 14:18:13 -0400

    close CamelReactiveStreamsService after use

--- service/application/src/main/java/com/solace/maas/ep/event/management/agent/config/VMRConfiguration.java
+++ service/application/src/main/java/com/solace/maas/ep/event/management/agent/config/VMRConfiguration.java
@@ -10,7 +10,6 @@ import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-import java.util.ArrayList;
 import java.util.List;
 import java.util.Properties;
 
