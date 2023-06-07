bdc985d24360fd1321c897cf9ec9b0858042f70b
shooshmand-sol <salman.hooshmand@solace.com>
2023-06-07 15:25:15 -0400

    refactor

--- .github/workflows/release_scripts/setup-ssh.sh
+++ .github/workflows/release_scripts/setup-ssh.sh
@@ -1,18 +1,18 @@
-echo "preparing maven settings file"
+echo "Preparing maven settings file"
 envsubst < settings-template.xml > "local-settings.xml"
 cp local-settings.xml ~/.m2/settings.xml
 
-echo "configure ssh key"
+echo "Configuring ssh key"
 mkdir -p ~/.ssh
 echo "$SSH_PRIVATE_KEY" > ~/.ssh/id_rsa
 chmod 600 ~/.ssh/id_rsa
 
-echo "Set StrictHostKeyChecking no"
+echo "Setting StrictHostKeyChecking no"
 echo -e "Host *\n\tStrictHostKeyChecking no\n\n" > ~/.ssh/config
 
-echo "Whitelist  github.com in ~/.ssh/known_hosts"
+echo "Whitelisting  github.com in ~/.ssh/known_hosts"
 ssh-keyscan -t rsa github.com >> ~/.ssh/known_hosts
 
-echo "Configure Git User"
+echo "Configuring Git User"
 git config user.email "actions@github.com"
 git config user.name "GitHub Actions"
