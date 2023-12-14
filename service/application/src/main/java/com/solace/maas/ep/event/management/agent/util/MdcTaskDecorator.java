fad64450d8ee3d5ca83dfd5e284e34bb89472bfa
Greg Meldrum <gregmeldrum@gmail.com>
2023-12-14 13:33:00 -0500

    DATAGO-67370: Add a thread pool to command manager (#142)

---
+++ service/application/src/main/java/com/solace/maas/ep/event/management/agent/util/MdcTaskDecorator.java
@@ -0,0 +1,25 @@
+package com.solace.maas.ep.event.management.agent.util;
+
+import org.slf4j.MDC;
+import org.springframework.core.task.TaskDecorator;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Optional;
+
+public class MdcTaskDecorator implements TaskDecorator {
+
+    @Override
+    public Runnable decorate(Runnable runnable) {
+        Map<String, String> contextMap = Optional.ofNullable(MDC.getCopyOfContextMap()).orElse(new HashMap<>());
+        return () -> {
+            try {
+                MDC.setContextMap(contextMap);
+                runnable.run();
+            } finally {
+                MDC.clear();
+            }
+
+        };
+    }
+}
