20a8b9087ccec5693fbe2229b02fc8445d9608c9
mynecker <jochen.traunecker@gmail.com>
2024-11-11 17:01:57 +0100

    fixed IT tests added unit test for TerraformUtils cleanup of config dir

--- service/application/src/test/java/com/solace/maas/ep/event/management/agent/TestConfig.java
+++ service/application/src/test/java/com/solace/maas/ep/event/management/agent/TestConfig.java
@@ -44,7 +44,6 @@ import java.util.Properties;
 import java.util.Random;
 import java.util.concurrent.TimeUnit;
 
-import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
