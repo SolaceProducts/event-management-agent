c1f6f979edcda01e164d3c0e18dc72cc24cb625e
mynecker <jochen.traunecker@gmail.com>
2024-11-12 11:53:37 +0100

    added IT and unit tests

--- service/application/src/test/java/com/solace/maas/ep/event/management/agent/subscriber/EmRestartSimulatingPersistentMessageHandlerObserverTest.java
+++ service/application/src/test/java/com/solace/maas/ep/event/management/agent/subscriber/EmRestartSimulatingPersistentMessageHandlerObserverTest.java
@@ -8,27 +8,43 @@ public class EmRestartSimulatingPersistentMessageHandlerObserverTest {
 
     @Test
     void testOnPhaseChangeRestartSimulated() {
-        EmaRestartSimulatingPersistentMessageHandlerObserver emaRestartSimulatingPersistentMessageHandlerObserver = new EmaRestartSimulatingPersistentMessageHandlerObserver();
+        EmaRestartSimulatingPersistentMessageHandlerObserver emaRestartSimulatingPersistentMessageHandlerObserver =
+                new EmaRestartSimulatingPersistentMessageHandlerObserver();
         emaRestartSimulatingPersistentMessageHandlerObserver.setRestartSimulated(true);
-        assertThat(emaRestartSimulatingPersistentMessageHandlerObserver.onPhaseChange(null, PersistentMessageHandlerObserverPhase.PRE_PROCESSOR_EXECUTION)).isFalse();
-        assertThat(emaRestartSimulatingPersistentMessageHandlerObserver.onPhaseChange(null, PersistentMessageHandlerObserverPhase.PRE_ACKNOWLEDGED)).isFalse();
-        assertThat(emaRestartSimulatingPersistentMessageHandlerObserver.onPhaseChange(null, PersistentMessageHandlerObserverPhase.ACKNOWLEDGED)).isTrue();
-        assertThat(emaRestartSimulatingPersistentMessageHandlerObserver.onPhaseChange(null, PersistentMessageHandlerObserverPhase.FAILED)).isTrue();
-        assertThat(emaRestartSimulatingPersistentMessageHandlerObserver.onPhaseChange(null, PersistentMessageHandlerObserverPhase.PROCESSOR_COMPLETED)).isTrue();
-        assertThat(emaRestartSimulatingPersistentMessageHandlerObserver.onPhaseChange(null, PersistentMessageHandlerObserverPhase.PROCESSING_INITIATED)).isTrue();
-        assertThat(emaRestartSimulatingPersistentMessageHandlerObserver.onPhaseChange(null, PersistentMessageHandlerObserverPhase.RECEIVED)).isTrue();
+        assertThat(emaRestartSimulatingPersistentMessageHandlerObserver
+                .onPhaseChange(null, PersistentMessageHandlerObserverPhase.PRE_PROCESSOR_EXECUTION)).isFalse();
+        assertThat(emaRestartSimulatingPersistentMessageHandlerObserver
+                .onPhaseChange(null, PersistentMessageHandlerObserverPhase.PRE_ACKNOWLEDGED)).isFalse();
+        assertThat(emaRestartSimulatingPersistentMessageHandlerObserver
+                .onPhaseChange(null, PersistentMessageHandlerObserverPhase.ACKNOWLEDGED)).isTrue();
+        assertThat(emaRestartSimulatingPersistentMessageHandlerObserver
+                .onPhaseChange(null, PersistentMessageHandlerObserverPhase.FAILED)).isTrue();
+        assertThat(emaRestartSimulatingPersistentMessageHandlerObserver
+                .onPhaseChange(null, PersistentMessageHandlerObserverPhase.PROCESSOR_COMPLETED)).isTrue();
+        assertThat(emaRestartSimulatingPersistentMessageHandlerObserver
+                .onPhaseChange(null, PersistentMessageHandlerObserverPhase.PROCESSING_INITIATED)).isTrue();
+        assertThat(emaRestartSimulatingPersistentMessageHandlerObserver
+                .onPhaseChange(null, PersistentMessageHandlerObserverPhase.RECEIVED)).isTrue();
     }
 
     @Test
     void testOnPhaseChangeRestartNotSimulated() {
-        EmaRestartSimulatingPersistentMessageHandlerObserver emaRestartSimulatingPersistentMessageHandlerObserver = new EmaRestartSimulatingPersistentMessageHandlerObserver();
+        EmaRestartSimulatingPersistentMessageHandlerObserver emaRestartSimulatingPersistentMessageHandlerObserver =
+                new EmaRestartSimulatingPersistentMessageHandlerObserver();
         emaRestartSimulatingPersistentMessageHandlerObserver.setRestartSimulated(false);
-        assertThat(emaRestartSimulatingPersistentMessageHandlerObserver.onPhaseChange(null, PersistentMessageHandlerObserverPhase.PRE_PROCESSOR_EXECUTION)).isTrue();
-        assertThat(emaRestartSimulatingPersistentMessageHandlerObserver.onPhaseChange(null, PersistentMessageHandlerObserverPhase.PRE_ACKNOWLEDGED)).isTrue();
-        assertThat(emaRestartSimulatingPersistentMessageHandlerObserver.onPhaseChange(null, PersistentMessageHandlerObserverPhase.ACKNOWLEDGED)).isTrue();
-        assertThat(emaRestartSimulatingPersistentMessageHandlerObserver.onPhaseChange(null, PersistentMessageHandlerObserverPhase.FAILED)).isTrue();
-        assertThat(emaRestartSimulatingPersistentMessageHandlerObserver.onPhaseChange(null, PersistentMessageHandlerObserverPhase.PROCESSOR_COMPLETED)).isTrue();
-        assertThat(emaRestartSimulatingPersistentMessageHandlerObserver.onPhaseChange(null, PersistentMessageHandlerObserverPhase.PROCESSING_INITIATED)).isTrue();
-        assertThat(emaRestartSimulatingPersistentMessageHandlerObserver.onPhaseChange(null, PersistentMessageHandlerObserverPhase.RECEIVED)).isTrue();
+        assertThat(emaRestartSimulatingPersistentMessageHandlerObserver
+                .onPhaseChange(null, PersistentMessageHandlerObserverPhase.PRE_PROCESSOR_EXECUTION)).isTrue();
+        assertThat(emaRestartSimulatingPersistentMessageHandlerObserver
+                .onPhaseChange(null, PersistentMessageHandlerObserverPhase.PRE_ACKNOWLEDGED)).isTrue();
+        assertThat(emaRestartSimulatingPersistentMessageHandlerObserver
+                .onPhaseChange(null, PersistentMessageHandlerObserverPhase.ACKNOWLEDGED)).isTrue();
+        assertThat(emaRestartSimulatingPersistentMessageHandlerObserver
+                .onPhaseChange(null, PersistentMessageHandlerObserverPhase.FAILED)).isTrue();
+        assertThat(emaRestartSimulatingPersistentMessageHandlerObserver
+                .onPhaseChange(null, PersistentMessageHandlerObserverPhase.PROCESSOR_COMPLETED)).isTrue();
+        assertThat(emaRestartSimulatingPersistentMessageHandlerObserver
+                .onPhaseChange(null, PersistentMessageHandlerObserverPhase.PROCESSING_INITIATED)).isTrue();
+        assertThat(emaRestartSimulatingPersistentMessageHandlerObserver
+                .onPhaseChange(null, PersistentMessageHandlerObserverPhase.RECEIVED)).isTrue();
     }
 }
