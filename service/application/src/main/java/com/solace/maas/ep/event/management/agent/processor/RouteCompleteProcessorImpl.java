8659dd8c40382bd18c5ec18f10ffc7269a4cb2d4
moodiRealist <40175386+moodiRealist@users.noreply.github.com>
2025-02-05 13:31:39 -0500

    [DATAGO-93766] update to use the correct constant

--- service/application/src/main/java/com/solace/maas/ep/event/management/agent/processor/RouteCompleteProcessorImpl.java
+++ service/application/src/main/java/com/solace/maas/ep/event/management/agent/processor/RouteCompleteProcessorImpl.java
@@ -15,7 +15,7 @@ import org.springframework.stereotype.Component;
 
 import java.util.concurrent.TimeUnit;
 
-import static com.solace.maas.ep.common.metrics.ObservabilityConstants.MAAS_EMA_CONFIG_PUSH_EVENT_CYCLE_TIME;
+import static com.solace.maas.ep.common.metrics.ObservabilityConstants.MAAS_EMA_SCAN_EVENT_CYCLE_TIME;
 import static com.solace.maas.ep.common.metrics.ObservabilityConstants.ORG_ID_TAG;
 
 @Slf4j
@@ -43,6 +43,10 @@ public class RouteCompleteProcessorImpl extends RouteCompleteProcessor {
         String scanId = (String) exchange.getIn().getHeader(RouteConstants.SCAN_ID);
         String orgId = (String) exchange.getIn().getHeader(RouteConstants.ORG_ID);
         String scanType = getScanType(exchange);
+
+        // ToDo: remove this before merging the PR
+        log.info("will2 save scan status as complete for scanId {} orgId {} scanType {}", scanId, orgId, scanType);
+
         scanStatusService.save(scanType, scanId, ScanStatus.COMPLETE);
         if (Boolean.TRUE.equals(eventPortalProperties.getManaged()) && scanService.isScanComplete(scanId)) {
             registerScanCycleTime(scanId, orgId);
@@ -53,12 +57,12 @@ public class RouteCompleteProcessorImpl extends RouteCompleteProcessor {
         ScanEntity scanEntity = scanService.getById(scanId);
         long now = System.currentTimeMillis();
         Timer jobCycleTime = Timer
-                .builder(MAAS_EMA_CONFIG_PUSH_EVENT_CYCLE_TIME)
+                .builder(MAAS_EMA_SCAN_EVENT_CYCLE_TIME)
                 .tag(ORG_ID_TAG, orgId)
                 .register(meterRegistry);
 
         // ToDo: remove this before merging the PR
-        log.debug("moodi xxx scanEntity {} now {} scanEntity.getCreatedAt().toEpochMilli() {} now-createdAt {}",
+        log.info("moodi2 xxx scanEntity {} now {} scanEntity.getCreatedAt().toEpochMilli() {} now-createdAt {}",
                 scanEntity, now, scanEntity.getCreatedAt().toEpochMilli(), now - scanEntity.getCreatedAt().toEpochMilli());
 
         jobCycleTime.record(now - scanEntity.getCreatedAt().toEpochMilli(), TimeUnit.MILLISECONDS);
